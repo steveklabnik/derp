e61ca16ba3fea2ce73daebc892497e7d948ea112
Steve Klabnik <steve@steveklabnik.com>
2011-11-09 17:25:13 -0500

    adding derp documentation

--- lib/derp.rb
+++ lib/derp.rb
@@ -1,3 +1,17 @@
+# This is the source code for the 'derp' gem.
+#
+# This gem is mostly silly. It's more of a 'hello world' kind
+# of gem; I really wanted to try out this particular stack, and so
+# it serves as an example of sorts in building a gem with minitest,
+# bundler, watchr, etc.
+#
+# As far as functionality goes, this gem monkeypatches `String` to add
+# a `to_derp` method, and a `from_derp` method. These encode the given
+# string in a form of morse code, using herp and derp for long and short.
+#
+# I told you it was dumb.
+
+# What's a gem without a monkeypatch?
 class String
   Morse = {
     'a' => 'herpderp',
@@ -29,6 +43,9 @@ class String
     ' ' => '.'
   }
 
+  # This method converts a string to a derp string. Not to be confused
+  # with a BenString, the derp string is simply a `String` with only
+  # 'herp' and 'derp' in it.
   def to_derp
     out = ""
     self.downcase.split(//).each do |c|
@@ -37,6 +54,8 @@ class String
     out.rstrip
   end
 
+  # The inverse of `to_derp`, `from_derp` turns a derp string back into a
+  # normal `String`.
   def from_derp
     inverted = Morse.invert
     out = ""
